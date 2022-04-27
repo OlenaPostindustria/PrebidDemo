95a84b61221c0602db1ad74c0aa8081880069bce
Olena Stepaniuk <olena.stepaniuk@postindustria.com>
2022-04-27 11:03:55 +0300

    Initial Commit

---
+++ PrebidDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrebidDemo
+//
+//  Created by Olena Stepaniuk on 27.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
